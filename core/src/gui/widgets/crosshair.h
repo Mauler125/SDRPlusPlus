c305dde3c0fcb5b45dfeac7d8339a92a205fbf50
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2025-11-14 21:35:01 +0100

    Fix compile error caused by missing include
    ImRect is defined in imgui_internal.h but this include was only included in crosshair.cpp. (I really need to rework the code to use a PCH, it will reduce compile times significantly as well and avoid these pesky errors for common code).

--- core/src/gui/widgets/crosshair.h
+++ core/src/gui/widgets/crosshair.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <imgui/imgui.h>
+#include <imgui/imgui_internal.h>
 
 namespace ImGui {
     void DrawCrosshairUnderCursor(const ImRect& clip, const ImU32 color = IM_COL32(255, 255, 255, 255), const float thickness = 1.0f);
