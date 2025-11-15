Mauler125/SDRPlusPlus

commit a908cb3dcd68fb39ecc1e2a635a07d4a16182718

Make frequency unit specifier more clear

core/src/gui/widgets/waterfall.cpp
@@ -50,16 +50,16 @@ inline double findBestRange(double bandwidth, int maxSteps) {
 inline void printAndScale(double freq, char* buf) {
     double freqAbs = fabs(freq);
     if (freqAbs < 1000) {
-        sprintf(buf, "%.6g", freq);
+        sprintf(buf, "%.6gHz", freq);
     }
     else if (freqAbs < 1000000) {
-        sprintf(buf, "%.6lgK", freq / 1000.0);
+        sprintf(buf, "%.6lgKHz", freq / 1000.0);
     }
     else if (freqAbs < 1000000000) {
-        sprintf(buf, "%.6lgM", freq / 1000000.0);
+        sprintf(buf, "%.6lgMHz", freq / 1000000.0);
     }
     else if (freqAbs < 1000000000000) {
-        sprintf(buf, "%.6lgG", freq / 1000000000.0);
+        sprintf(buf, "%.6lgGHz", freq / 1000000000.0);
     }
 }
 

commit 1b9ac596ea99980103c06aa1e086e25f616cfd19

Fix tooltip formatting
Small regression since a908cb3dcd68fb39ecc1e2a635a07d4a16182718

core/src/gui/widgets/waterfall.cpp
@@ -508,9 +508,9 @@ namespace ImGui {
                     if (ImGui::GetIO().KeyCtrl) {
                         ImGui::Separator();
                         printAndScale(_vfo->generalOffset + centerFreq, buf);
-                        ImGui::Text("Frequency: %sHz", buf);
+                        ImGui::Text("Frequency: %s", buf);
                         printAndScale(_vfo->bandwidth, buf);
-                        ImGui::Text("Bandwidth: %sHz", buf);
+                        ImGui::Text("Bandwidth: %s", buf);
                         ImGui::Text("Bandwidth Locked: %s", _vfo->bandwidthLocked ? "Yes" : "No");
 
                         float strength, snr;