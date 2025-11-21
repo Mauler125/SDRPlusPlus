37ded98d35f095c558aa4b32a19ba8119e3e784a
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2025-11-21 18:00:45 +0100

    Cleanup crosshair header
    Move enum outside namespace to adhere to Dear ImGui coding/api style.

--- core/src/gui/widgets/crosshair.h
+++ core/src/gui/widgets/crosshair.h
@@ -2,16 +2,17 @@
 #include <imgui/imgui.h>
 #include <imgui/imgui_internal.h>
 
+enum ImGuiCrosshairFlags_ {
+    ImGuiCrosshairFlags_None = 0,
+    ImGuiCrosshairFlags_CullHorizontal = 1 << 0,
+    ImGuiCrosshairFlags_CullVertical = 1 << 1,
+
+    ImGuiCrosshairFlags_CullAllMask = ImGuiCrosshairFlags_CullHorizontal | ImGuiCrosshairFlags_CullVertical,
+};
+
+typedef int ImGuiCrosshairFlags;
+
 namespace ImGui {
-    enum ImGuiCrosshairFlags_ {
-        ImGuiCrosshairFlags_None = 0,
-        ImGuiCrosshairFlags_CullHorizontal = 1 << 0,
-        ImGuiCrosshairFlags_CullVertical = 1 << 1,
-
-        ImGuiCrosshairFlags_CullAllMask = ImGuiCrosshairFlags_CullHorizontal | ImGuiCrosshairFlags_CullVertical,
-    };
-
-    typedef int ImGuiCrosshairFlags;
     void DrawCrosshairUnderCursor(const ImRect& clip, const ImU32 color = IM_COL32(255, 255, 255, 255), 
         const float thickness = 1.0f, const ImGuiCrosshairFlags flags = ImGuiCrosshairFlags_None);
 }
