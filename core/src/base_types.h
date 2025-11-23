110035b0c50a48d0b8f1088deebc28f9fbb279ad
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2025-11-23 21:51:25 +0100

    Make s64 string buffer size correct
    Can be a byte smaller.

--- core/src/base_types.h
+++ core/src/base_types.h
@@ -16,7 +16,7 @@
 #define I32_STR_BUF_SIZE 12
 
 // Sufficient for a signed 64 bit integer [ -9223372036854775807 to +9223372036854775807 ] + '\0'.
-#define I64_STR_BUF_SIZE 22
+#define I64_STR_BUF_SIZE 21
 
 // Sufficient for a 64 bit pointer [ ffffffffffffffff ] + '\0'.
 #define PTR_STR_BUF_SIZE 17
