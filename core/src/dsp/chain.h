48761a4b78069f8b187424e88c7b89c9fe4c08d3
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2025-12-10 16:54:19 +0100

    Properly implement chain::blockBefore()

--- core/src/dsp/chain.h
+++ core/src/dsp/chain.h
@@ -163,10 +163,10 @@ namespace dsp {
 
     private:
         Processor<T, T>* blockBefore(Processor<T, T>* block) {
-            // TODO: This is wrong and must be fixed when I get more time
+            Processor<T, T>* lastEnabled = NULL;
             for (auto& ln : links) {
-                if (ln == block) { return NULL; }
-                if (states[ln]) { return ln; }
+                if (ln == block) { return lastEnabled; }
+                if (states[ln]) { lastEnabled = ln; }
             }
             return NULL;
         }
