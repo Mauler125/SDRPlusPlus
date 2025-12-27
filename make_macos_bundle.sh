6687366916caea75ef66d54251e05ed22b7228e3
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2025-12-27 23:00:59 +0100

    Fix incorrect lib name during copy
    lib has been renamed to just core.

--- make_macos_bundle.sh
+++ make_macos_bundle.sh
@@ -28,7 +28,7 @@ bundle_create_plist sdrpp SDR++ org.sdrpp.sdrpp 1.2.1 sdrp sdrpp sdrpp $BUNDLE/C
 
 # Core
 bundle_install_binary $BUNDLE $BUNDLE/Contents/MacOS $BUILD_DIR/sdrpp 
-bundle_install_binary $BUNDLE $BUNDLE/Contents/Frameworks $BUILD_DIR/core/libsdrpp_core.dylib
+bundle_install_binary $BUNDLE $BUNDLE/Contents/Frameworks $BUILD_DIR/core/lib_core.dylib
 
 # Source modules
 bundle_install_binary $BUNDLE $BUNDLE/Contents/Plugins $BUILD_DIR/source_modules/airspy_source/airspy_source.dylib
